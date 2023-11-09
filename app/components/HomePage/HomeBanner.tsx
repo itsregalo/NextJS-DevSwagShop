40e7379dca19d71ce91995837e2eb4e3c34306fa
itsregalo <itsregalo047@gmail.com>
2023-11-09 17:22:00 +0300

    banner and footer with daisy Ui

---
+++ app/components/HomePage/HomeBanner.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function HomeBanner() {
+  return (
+    <div className="home-banner p-20"
+      style={{color:"#130e01"}}
+    >
+        <div className="home-banner-content text-center">
+            <h1 className="text-2xl md:text-5xl">
+                         Welcome Dev Swag Shop</h1>
+            <p className="text-l md:text-2xl">One Stop Swag Shop, Code With Passion</p>
+        </div>
+    </div>
+  )
+}
